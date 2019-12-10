1fd1e1db7144f809c7813631ca2a0b9427cc5acb
Hugo Villegas <hugo.villegas@mrm-mccann.de>
2019-12-10 10:07:20 +0100

    Finished Chapter 2 "Essential Directives, Options, and Tools" https://www.linkedin.com/learning/learning-vue-js-3/rendering-elements-conditionally Installed Browser Sync

--- scripts.js
+++ scripts.js
@@ -1,26 +1,49 @@
-// NASA:
-var url = "https://api.nasa.gov/planetary/apod?api_key=0bKlsZEkuHkAi9eZOMZXTx5LcvfrGDuRfnSOSt5w";
-
 var vm = new Vue ({
     el: "#app",
     data: {
-        greetings: "Learning Vue.js",
-        email: "yourmail@example.com",
-        submitted: false,
-        imgSrc: "",
-        imgTitle: "Hola Cabron"
+        asteroids: []
+    },
+    computed: {
+        numAsteroids: function () {
+            return this.asteroids.length;
+        },
+        closestObject: function () {
+            var neosHavingData = this.asteroids.filter(function (neo) {
+                return neo.close_approach_data.length > 0;
+            });
+            var simpleNeos = neosHavingData.map( function (neo) {
+                return { name: neo.name, miles: neo.close_approach_data[0].miss_distance_miles };
+            });
+            var sortedNeos = simpleNeos.sort( function ( a, b) {
+                return a.miles - b.miles;
+            })
+            return sortedNeos[0].name;
+        }
+    },
+    // Wrap our Ajax request in a function with a meaningful name,
+    // and make it clear that we want it to be executed as soon as the Vue instance is created.
+    // https://vuejs.org/v2/guide/instance.html#Lifecycle-Diagram
+    created: function () {
+        this.fetchAsteroids();
     },
     methods: {
-        process: function () {
-            this.submitted = true;
+        fetchAsteroids: function () {
+            // var url = "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=0bKlsZEkuHkAi9eZOMZXTx5LcvfrGDuRfnSOSt5w";
+            var url = "http://737798.youcanlearnit.net/neos.json";
+            axios.get(url)
+                .then(function (res) {
+                    vm.asteroids = res.data.near_earth_objects.slice(0, 10);
+                })
+        },
+        getCloseApproachDate: function (a) {
+            if (a.close_approach_data.length > 0) {
+                return a.close_approach_data[0].close_approach_date;
+            }
+            return 'N/A';
+        },
+        remove: function ( index ) {
+            this.asteroids.splice(index, 1);
         }
     }
 });
-
-axios.get(url)
-    .then(function (res) {
-            console.log(res);
-            vm.imgSrc = res.data.url;
-            vm.imgTitle = res.data.title;
-    });
 
